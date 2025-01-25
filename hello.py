f5d2bfb2d7e068dd88e02ce83c6619e9a8240ab5
PritZalavadiya97 <53326100+PritZalavadiya97@users.noreply.github.com>
2025-01-25 22:24:32 +0530

    my python program

---
+++ hello.py
@@ -0,0 +1,80 @@
+"""
+print("Hello World!!")
+print("My age 26")
+print(1997)
+print(2025-1997)
+"""
+
+name = "Prit Zalavadiya"
+currentYear = 2025
+bornYear = 1997
+age = currentYear - bornYear
+a = "10.3"
+b = 2.5
+
+
+print(type(name))
+print(currentYear / bornYear)
+print("My Age is :", age)
+
+# Arithmetic Operators
+"""
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a ** b)
+print(a % b)
+"""
+
+# Relation Operators
+"""
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
+"""
+
+#Assessment Operators
+"""
+a += 2
+a -= 2
+a /= 2
+a *= 2
+a %= 2
+a **= 2
+print(a)
+"""
+
+#Types Conversion
+"""
+sum = float(a) + b
+c = float(a)
+print(sum)
+print(c)
+"""
+
+
+"""
+num_a = input("Enter Your A Number: ")
+num_b = input("Enter Your B Number: ")
+
+# Convert the inputs to integers (or float if needed)
+num_a = int(num_a)
+num_b = int(num_b)
+
+# Calculate the sum
+result = num_a >= num_b
+
+print("Your A Number is", num_a)
+print("Your B Number is", num_b)
+print("The Condition of A >= B is:", result)
+"""
+
+"""
+side = float(input("Entry Your Side: "))
+squareArea = (side ** 2)
+print("Your Square Area is", squareArea)
+"""
